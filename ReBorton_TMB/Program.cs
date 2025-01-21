7ce0ec85ee401bb6316b779a133683bddfec1c52
tatarmb4s <tatar.matyas.bence4s@gmail.com>
2025-01-21 17:46:19 +0100

    A program indítása
    Signed-off-by: tatarmb4s <tatar.matyas.bence4s@gmail.com>

--- ReBorton_TMB/Program.cs
+++ ReBorton_TMB/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Borton_Lib.Classes;
+using ReBorton_TMB;
+
+Console.Title = "Börtönkezelő TMB (KonZol)";
+
+// Például a futtatható mappában lévő data.json
+string dataFilePath = "data.json";
+
+// Létrehozzuk a börtönkezelőt
+BortonKezelo kezelo = new BortonKezelo();
+
+// Betöltjük, ha van
+kezelo.BetoltesJsonbol(dataFilePath);
+
+// Elindítjuk a menüt
+var menu = new BortonMenu(kezelo, dataFilePath);
+menu.Run();
+
+// A program sose fusson hibára, mindent try-catch-csel kezelünk a menü oldalon
+// Tehát kilépés
+Console.WriteLine("Kilépés...");
