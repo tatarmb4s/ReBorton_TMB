179ada4d38e6374b7d54b4dd6501af1ef8aa878d
tatarmb4s <tatar.matyas.bence4s@gmail.com>
2025-01-21 17:15:14 +0100

    Exceptions
    Signed-off-by: tatarmb4s <tatar.matyas.bence4s@gmail.com>

---
+++ Borton_Lib/Exceptions/BortonException.cs
@@ -0,0 +1,26 @@
+﻿namespace Borton_Lib.Exceptions
+{
+    /// <summary>
+    /// A börtön logikában fellépő kivételekhez használt egyedi kivétel
+    /// </summary>
+    public class BortonException : Exception
+    {
+        /// <summary>
+        /// Alapértelmezett konstruktor
+        /// </summary>
+        public BortonException() { }
+
+        /// <summary>
+        /// Üzenettel ellátott kivétel
+        /// </summary>
+        /// <param name="message">Hibaüzenet</param>
+        public BortonException(string message) : base(message) { }
+
+        /// <summary>
+        /// Üzenettel és belső kivétellel ellátott konstruktor
+        /// </summary>
+        /// <param name="message">Hibaüzenet</param>
+        /// <param name="innerException">Belső kivétel</param>
+        public BortonException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
