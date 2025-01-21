9eabd331f9c87f0157135c5f4d980137b3c453ed
tatarmb4s <tatar.matyas.bence4s@gmail.com>
2025-01-21 17:39:08 +0100

    IO
    Signed-off-by: tatarmb4s <tatar.matyas.bence4s@gmail.com>

--- ReBorton_TMB/BortonJsonIO.cs
+++ ReBorton_TMB/BortonJsonIO.cs
@@ -2,9 +2,61 @@
 using Borton_Lib.Classes;
 
 
-namespace ReBorton_TMB
+namespace ReBorton_TMB.IO
 {
-    internal class BortonJsonIO
+    /// <summary>
+    /// A JSON beolvasásra és mentésre szolgáló segédosztály.
+    /// </summary>
+    public static class BortonJsonIO
     {
+        /// <summary>
+        /// Segéd struktúra a JSON adatok deszerializálására
+        /// (például 1 tulajdonos + a hozzátartozó cellák, rabok, őrök paraméterei).
+        /// </summary>
+        public class InitialData
+        {
+            public Tulajdonos Tulajdonos { get; set; }
+            // Egyéb mezők is lehetnek, pl. Börtönőrök, Rabok, Cellák, ha úgy akarjuk,
+            // de a Tulajdonos konstruktora maga is létrehozza a Borton-t.
+        }
+
+        /// <summary>
+        /// Betölti a kezdeti adatokat a megadott JSON fájlból.
+        /// </summary>
+        /// <param name="filePath">A JSON fájl elérési útvonala.</param>
+        /// <returns>Az InitialData objektum.</returns>
+        public static InitialData BetolKezdetiAdatok(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    // Ha a fájl nem létezik, visszaadunk egy defaultot, vagy dobunk kivételt.
+                    Console.WriteLine("[FIGYELMEZTETÉS] A JSON fájl nem található, üres adatokat hozunk létre!");
+                    var dummyBorton = new Borton("AlapBorton", null);
+
+                    return new InitialData
+                    {
+                        Tulajdonos = new Tulajdonos(1, "AlapTulaj", Borton_Lib.Enums.Neme.Ferfi, dummyBorton)
+                    };
+                }
+
+                string jsonContent = File.ReadAllText(filePath);
+                var initialData = JsonSerializer.Deserialize<InitialData>(jsonContent,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return initialData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[HIBA] Nem sikerült betölteni a kezdeti adatokat: " + ex.Message);
+                // Ha hiba van, generáljunk valami alapértelmezettet
+                var dummyBorton = new Borton("AlapBorton", null);
+
+                return new InitialData
+                {
+                    Tulajdonos = new Tulajdonos(1, "AlapTulaj", Borton_Lib.Enums.Neme.Ferfi, dummyBorton)
+                };
+            }
+        }
     }
 }
